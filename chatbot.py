920441b1e33a6fb44c65634db126234d9fa3064f
infra <infra@sterile.solutions>
2022-01-27 18:43:58 -0500

    add invocation

--- chatbot.py
+++ chatbot.py
@@ -9,6 +9,9 @@ from aiohttp import web
 # rot13 fx-SEUE0Tz7Dst84pSZle5SG3OyoxSWxsebtPOSLzRTuyQz6h6H
 openai.api_key = os.getenv("OPENAI_API_KEY", "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")
 
+# invoked with
+# pipenv run sh -c 'SIGNAL_CLI_PATH=$PWD/auxin-cli ADMIN=+15133278483 SIGNAL_CLI=auxin-cli NO_MEMFS=1 NO_DOWNLOAD=1 BOT_NUMBER=+12696666006 PAUTH=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTI2OTQ3MywiZXhwIjoxOTU2ODQ1NDczfQ.5aqlyajbwsi7Klf4BsO3cA9BzMXVV-qELy42HRKFSjM python3 chatbot.py'
+
 default_prompt = (
             "The following is a conversation with a helpful assistant and manager. "
             "The assistant is helpful, creative, clever, funny, very friendly, from Kalamazoo, Michigan, an artist and anarchist\n\n"
