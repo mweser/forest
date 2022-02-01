0d5e6507e6c6c2eff06ff284519c22b353136ed0
Olivia Schaefer <taygetea@gmail.com>
2022-01-31 21:45:01 -0500

    formatting, ulids

--- repostbot.py
+++ repostbot.py
@@ -10,6 +10,10 @@ from prometheus_async.aio import time
 from prometheus_client import Summary
 from forest.core import Bot, Message, run_bot, Response
 from forest.pdictng import aPersistDict
+from ulid2 import generate_ulid_as_base32
+from ulid2 import get_ulid_time, get_ulid_timestamp
+import datetime
+from typing import Any, AsyncIterator, Callable, Optional, Type, Union
 
 
 class ReplayBot(Bot):
@@ -27,25 +31,28 @@ class ReplayBot(Bot):
             return "signal session reset"
 
         if len(message.full_text): # full_text can be empty during receipts and typing indicators
-            print("fulltext:", message.full_text)
             if message.source.startswith('+'):
                 source = message.source[1:]
             else:
                 source = message.source
             timestamp = message.timestamp
-            message_key = str(source)+str(timestamp)
+            # message_key = str(source)+str(timestamp)
+            message_key = generate_ulid_as_base32(message.timestamp/1000)
+            self.humantime = get_ulid_time(message_key).strftime("%d-%b-%Y (%H:%M:%S.%f)")
             self.keyslist.append(message_key)
             self.messages[message_key] = message.full_text
-        return await self.default(message)
-        #return await super().handle_message(message)
+            
 
-    async def do_hello(self, _: Message) -> str:
-        return "Hello, world!"
+    async def ulid_to_human(self, ulid):
+        return get_ulid_time(ulid).strftime("%d-%b-%Y (%H:%M:%S.%f)")
 
     async def do_replay(self, _: Message) -> str:
         d = self.messages.dict_
         # return await [(key, val) for val in self.messages.get(keys)]
-        return d
+        output_text = ""
+        for val in list(d.keys()):
+            output_text += f"key: {val},\n    time: {await self.ulid_to_human(val)},\n    val: {d[val]}\n"
+        return output_text # [d[item] for item in list(d.keys())[0:3]]
 
     async def delete_from_pdict(self, pdict, key):
         await pdict.remove(key)
@@ -61,4 +68,5 @@ class ReplayBot(Bot):
 
 if __name__ == "__main__":
     run_bot(ReplayBot)
+    
 
