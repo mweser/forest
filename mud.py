40b7fe68abfd3842805ce9625d6ab86e74e2be87
cxloe <cxl0e@protonmail.com>
2022-02-07 19:36:41 -0500

    websockets are much better then telnet

--- mud.py
+++ mud.py
@@ -5,20 +5,17 @@
 from forest.core import Bot, Message, run_bot, Response
 import os
 from os import system
-from telnetlib import Telnet
-
-HOST = "66.42.116.114"
-
+from websocket import create_connection
+import time
+import rel
 
 class MudBot(Bot):
-    def __init__(self):
-        self.tn = Telnet.open(HOST, 4000)
-
     async def do_mud(self, msg: Message) -> Response:
         msg_text = msg.full_text
-        tn.write(msg_text.encode("ascii"))
-        response = tn.read_all()
-        return response.decode()
+        ws = create_connection("ws://66.42.116.114:4002")
+        response = ws.recv()
+        ws.close()
+        return response
 
 
 if __name__ == "__main__":
