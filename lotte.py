0a4b167cb85ce649524aca57e8d90858f447b41f
transfaeries <contact@transfaerie.com>
2022-02-19 20:29:53 -0500

    commit

--- lotte.py
+++ lotte.py
@@ -1,22 +1,24 @@
-from cmd import PROMPT
-import logging
-from urllib import request
-from forest import utils
-from forest.core import Message, PayBot, Response, hide, QuestionBot, requires_admin, run_bot
-import openai
+from datetime import datetime
 import os
-from aiohttp import web
+import logging
+from urllib import response
+from forest.core import Message, PayBot, Response, run_bot
+import openai
+
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY", "")
 
 class Lotte(PayBot):
+    """A narrative oriented GPT chatbot with semipersistent memory"""
 
     bios = {"lotte":("The following is a conversation with an AI assistant named Lotte. "
                 "Lotte is a young girl who is helpful, creative, clever, funny, very friendly, a writer and anarcho-communist. Lotte's older sister is called Imogen and she's an artist. Lotte's best friend is Rowena the faerie.\n\n"
                 "SOURCE: Hello, who are you?\nLotte: My name is Lotte, I'm an AI that loves having rivetting intellectual discussions. How can I help you today?"
                 # "SOURCE: MESSAGE \nLotte: "
             ),"rowena":("The following is a conversation with a faerie named Rowena. "
-                "Rowena is a faerie with mushroom like appearance, gossamer wings and a pointy tail. She likes to play tricks on people, and whilst she might come across as mean she really wants everyone to have fun. Rowena's best friend is Lotte.\n\n"
+                "Rowena is a faerie with mushroom like appearance, gossamer wings and a pointy tail."
+                "She likes to play tricks on people, and whilst she might come across as mean she really wants everyone to have fun. Rowena's best friend is Lotte.\n\n"
                 "SOURCE: Hello, who are you?\nRowena: My name is Rowena, I'm a faerie sort of creature, what sort of creature are you?"
                 # "SOURCE: MESSAGE \nRowena: "
             ),
@@ -24,10 +26,20 @@ class Lotte(PayBot):
 
     current_fronter="lotte"
 
-    conversation=[]
+    conversation={}
+
+    # 0 means it's free, 1 means it's occupied
+    namepool = {"Taylor":0,
+                "Morgan":0,
+                "Lee":0,
+                "Rhianon":0,
+                "Georgie": 0,
+                "Moritz": 0
+                }
     
     
     async def chat(self, msg: Message, fronter: str) -> Response:
+        """ Organises the conversation and sends responses """
 
         prompt = self.bios[fronter].replace("SOURCE",msg.source)
         for line in self.conversation:
@@ -35,11 +47,7 @@ class Lotte(PayBot):
         prompt = prompt + "\n" + f"{msg.source}: {msg.text} \n{fronter}: "
         self.conversation.append(f"{msg.source}: {msg.text}")
         
-
-        
-        
-
-        response = openai.Completion.create(  # type: ignore
+        result = openai.Completion.create(  # type: ignore
             engine="curie",
             prompt = prompt,
             temperature=0.9,
@@ -49,24 +57,33 @@ class Lotte(PayBot):
             presence_penalty=0.6,
             stop=["\n", f"{msg.source}:", fronter],
         )
-        answer= response["choices"][0]["text"].strip().replace("AI:", "\nAI:").replace(f"{msg.source}:", f"\n{msg.source}:")
+        answer= result["choices"][0]["text"].strip().replace("AI:", "\nAI:").replace(f"{msg.source}:", f"\n{msg.source}:")
 
         fronter = fronter[0].upper()+fronter[1:] #names being properly uppercase might help the prompt
 
-        self.conversation.append(f"{fronter}: {answer}")
+        self.conversation.append[]
         logging.info("conversation:")
         for line in self.conversation:
             logging.info(line)
 
         return answer # + f"\n and here's the extra:\n {str(len(self.conversation))} \n {self.conversation[len(self.conversation)-1]}"
 
-    async def do_rowena(self,msg:Message) -> str:
-        """Chat With Rowena the Faerie"""
-        return await self.chat(msg,"Rowena")
+    async def generate(self, prompt:str) -> Response:
+        """makes a request to the GPT model"""
 
-    async def do_lotte(self,msg:Message) -> str:
+        
+
+        return response
+        
+
+
+    async def do_rowena(self,msg:Message) -> Response:
+        """Chat With Rowena the Faerie"""
+        return await self.chat(msg,"rowena")
+
+    async def do_lotte(self,msg:Message) -> Response:
         """Chat With Lotte the AI"""
-        return await self.chat(msg,"Lotte")
+        return await self.chat(msg,"lotte")
 
     async def do_switch(self,msg:Message) -> Response:
         requested_fronter=msg.arg1.lower()
