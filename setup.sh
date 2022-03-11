83d1407ed01466eab07fcaa1ec6efa9a1bdc1262
cxloe <cxl0e@protonmail.com>
2022-03-11 18:32:31 -0500

    switch to poetry

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 echo "Installing dependencies.."
 sudo apt update
 sudo apt install python3.9 python3.9-dev python3-pip
-python3.9 -m pip install pipenv
-pipenv install
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+poetry install
 wget https://github.com/AsamK/signal-cli/releases/download/v0.10.3/signal-cli-0.10.3-Linux.tar.gz
 tar -xvf signal-cli-0.10.3-Linux.tar.gz
 ln -s ./signal-cli-0.10.3/bin/signal-cli .
-pip3 install InquirerPy
-python3 wizard.py
+poetry add InquirerPy
+poetry add rich
+poetry run python3 wizard.py
 
