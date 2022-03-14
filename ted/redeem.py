00d932c08fbcb6e2ccfe096518ee45e532ee8561
technillogue <technillogue@gmail.com>
2022-03-14 17:04:03 -0400

    regularize

--- ted/redeem.py
+++ ted/redeem.py
@@ -3,6 +3,11 @@ import aioredis
 from forest import utils
 
 
+def regularize(code: str) -> str:
+    text = "".join(code.upper()).encode("ascii", errors="ignore").decode()
+    return "".join(c if (c.isalpha()) else "" for c in text)
+
+
 class Redeem:
     def __init__(self) -> None:
         self.conn = aioredis.Redis(
@@ -19,7 +24,8 @@ class Redeem:
     async def check(self, user: str, code: str) -> bool:
         if await self.conn.hincrby("user_guesses", user, 1) > 3:
             return False
-        if await self.conn.hget("active_codes", code.upper()):
-            await self.conn.hset("active_codes", code.upper(), "USED")
+        regular_code = regularize(code)
+        if await self.conn.hget("active_codes", regular_code):
+            await self.conn.hset("active_codes", regular_code, "USED")
             return True
         return False
