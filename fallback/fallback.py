877808c2eed5b9912be2f809d574f2b234317f91
deepfates <deepfates@gmail.com>
2022-04-19 11:16:24 -0600

    use internal fly addresses to run prod

--- fallback/fallback.py
+++ fallback/fallback.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2021 MobileCoin Inc.
 # Copyright (c) 2021 The Forest Team
 import aiohttp
+import logging
 from typing import Optional
 
 from forest.core import Bot, Message, Response, run_bot
@@ -12,22 +13,38 @@ class FallbackBot(Bot):
         self, bot_number: Optional[str] = None, phrases: Optional[list[str]] = None
     ) -> None:
         self.session = aiohttp.client.ClientSession()
-
+        self.server = None
         if not phrases:
             with open("phrases.txt", "r") as file:
                 phrases = file.readlines()
         self.phrases = phrases
-        print(self.phrases)
+        logging.debug(self.phrases)
         super().__init__(bot_number)
 
-    async def fetch(self, url, data) -> Response:
+    async def post(self, url, data) -> Response:
         async with self.session.post(url, json=data) as response:
-            return await response.json()
+            return await response.json(content_type=None)
 
     async def do_fallback(self, msg: Message) -> str:
+        if not msg.text:
+            return ""
         data = {"texts": self.phrases, "query": msg.text, "top_k": 1}
-        result = await self.fetch("http://localhost:8080/search", data)
-        return result
+        if self.server == None:
+            for host in [
+                "http://semantic-search.internal:8080/search",
+                "http://localhost:8080/search",
+            ]:
+                try:
+                    result = await self.post(host, data)
+                    self.server = host
+                    return result
+                except Exception as e:
+                    logging.debug(e)
+                    pass
+        else:
+            result = await self.post(self.server, data)
+            return result
+        return "No acrossword server found"
 
 
 if __name__ == "__main__":
